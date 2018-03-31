aa7ae6d5b7e7092220de8b9c60a8611dad7ece1f
ramazan yaraliev <yaru@bk.ru>
2018-03-31 22:43:44 +0300

    send message from front to telegram by bot

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -11,5 +11,7 @@ class ChatChannel < ApplicationCable::Channel
     ActionCable.server.broadcast 'telegram',
                                  message: data['message'],
                                  chat_id: data['chat_id']
+
+    Telegram.bot.send_message(chat_id: data['chat_id'], text: data['message'])
   end
 end
