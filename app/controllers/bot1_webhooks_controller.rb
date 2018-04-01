d29abb246f1165c5cc720334907d5b9f769f59d3
root <yaru@bk.ru>
2018-04-01 21:45:50 +0300

    add module Bot

--- app/controllers/bot1_webhooks_controller.rb
+++ app/controllers/bot1_webhooks_controller.rb
@@ -2,29 +2,8 @@ class Bot1WebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   context_to_action!
 
+  @bot = 'robo1bot'
 
-  def start(data = nil, *)
-    unless User.exists?(chat_id: from['id'])
-      user = User.new(
-          chat_id: from['id'],
-          first_name: from['first_name'])
-      user.save
+  include Bots
 
-      ActionCable.server.broadcast 'telegram', message: "New user: #{from['first_name']}"
-    end
-
-    respond_with :message, text: "Hello, #{from['first_name']}"
-  end
-
-
-
-  def message(message)
-    ActionCable.server.broadcast 'telegram',
-                                 message: "#{message['from']['first_name']}: #{message['text']}",
-                                 chat_id: message['from']['id']
-
-    user = User.where(chat_id: message['from']['id']).first
-    message = user.messages.create(message: message['text'], bot: 'robo1bot')
-    message.save
-  end
 end
---
+++ app/controllers/bots.rb
@@ -0,0 +1,26 @@
+module Bots
+  def start(data = nil, *)
+    unless User.exists?(chat_id: from['id'])
+      user = User.new(
+          chat_id: from['id'],
+          first_name: from['first_name'])
+      user.save
+
+      ActionCable.server.broadcast 'telegram', message: "New user: #{from['first_name']}"
+    end
+
+    respond_with :message, text: "Hello, #{from['first_name']}"
+  end
+
+
+
+  def message(message)
+    ActionCable.server.broadcast 'telegram',
+                                 message: "#{message['from']['first_name']}: #{message['text']}",
+                                 chat_id: message['from']['id']
+
+    user = User.where(chat_id: message['from']['id']).first
+    message = user.messages.create(message: message['text'], bot: @bot)
+    message.save
+  end
+end
