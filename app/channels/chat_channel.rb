3c7e0d95c6ddbfff17cd687519210ee5a324fd84
ramazan yaraliev <yaru@bk.ru>
2018-04-01 15:30:25 +0300

    fix display 'me: '

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -8,16 +8,17 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def reply(data)
-    msg = data['message'].split[1]
+    msg = data['message']
+    chat_id = data['chat_id']
 
     ActionCable.server.broadcast 'telegram',
                                  message: msg,
-                                 chat_id: data['chat_id']
+                                 chat_id: chat_id
 
     Telegram.bot.send_message(chat_id: data['chat_id'], text: data['message'])
 
-    user = User.find(1)
-    message = user.messages.create(incoming: false, message: msg)
+    user = User.find(chat_id)
+    message = user.messages.create(incoming: false, message: msg.split[1])
     message.save
   end
 end
