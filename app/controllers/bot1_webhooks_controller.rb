88dc5ed8c441b64112d9d32552fd45756e18288f
ramazan <yaru@bk.ru>
2018-04-03 11:30:20 +0300

    shift module's code to classes

--- app/controllers/bot1_webhooks_controller.rb
+++ app/controllers/bot1_webhooks_controller.rb
@@ -4,6 +4,29 @@ class Bot1WebhooksController < Telegram::Bot::UpdatesController
 
   @bot = 'robo1bot'
 
-  include Bots
+  def start(data = nil, *)
+    unless Chat.exists?(chat_id: from['id'])
+      chat = Chat.new(
+          chat_id: from['id'],
+          first_name: from['first_name'])
+      chat.save
+
+      ActionCable.server.broadcast 'telegram', message: "New user: #{from['first_name']}"
+    end
+
+    respond_with :message, text: "Hello, #{from['first_name']}"
+  end
+
+
+
+  def message(message)
+    ActionCable.server.broadcast 'telegram',
+                                 message: "#{message['from']['first_name']}: #{message['text']}",
+                                 chat_id: message['from']['id']
+
+    chat = Chat.where(chat_id: message['from']['id']).first
+    message = chat.messages.create(message: message['text'], bot: @@bot)
+    message.save
+  end
 
 end
--- app/controllers/bot2_webhooks_controller.rb
+++ app/controllers/bot2_webhooks_controller.rb
@@ -4,5 +4,28 @@ class Bot2WebhooksController < Telegram::Bot::UpdatesController
 
   @bot = 'robo6bot'
 
-  include Bots
+  def start(data = nil, *)
+    unless Chat.exists?(chat_id: from['id'])
+      chat = Chat.new(
+          chat_id: from['id'],
+          first_name: from['first_name'])
+      chat.save
+
+      ActionCable.server.broadcast 'telegram', message: "New user: #{from['first_name']}"
+    end
+
+    respond_with :message, text: "Hello, #{from['first_name']}"
+  end
+
+
+
+  def message(message)
+    ActionCable.server.broadcast 'telegram',
+                                 message: "#{message['from']['first_name']}: #{message['text']}",
+                                 chat_id: message['from']['id']
+
+    chat = Chat.where(chat_id: message['from']['id']).first
+    message = chat.messages.create(message: message['text'], bot: @@bot)
+    message.save
+  end
 end
