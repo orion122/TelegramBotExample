bcd52666bace901f7aec629419b05452a0f9c187
ramazan <yaru@bk.ru>
2018-04-05 12:58:16 +0300

    add validates_presence_of

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,4 @@
 class Chat < ApplicationRecord
+  validates_presence_of :chat_id, :first_name
   has_many :messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
+  validates_presence_of :chat_id, :message
   belongs_to :chat
 end
