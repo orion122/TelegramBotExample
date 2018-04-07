9003d55e83259e559763bb3fbc1afd7ca251fe44
ramazan yaraliev <yaru@bk.ru>
2018-04-07 22:36:54 +0300

    edit inde and show; add GET root_url

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -3,18 +3,31 @@ require 'rails_helper'
 RSpec.describe ChatsController, type: :controller do
 
   describe "GET #index" do
-    it "returns success response" do
+    it "returns index template" do
       get :index
-      expect(response).to be_success
+      expect(response).to render_template("index")
     end
   end
 
 
   describe "GET #show" do
     let(:chat) { Chat.create(chat_id: "123456", first_name: "Pavel") }
+
     it "returns success response" do
       get :show, params: { id: chat }
       expect(response).to be_success
+    end
+
+    it "render show template" do
+      get :show, params: { id: chat }
+      expect(response).to render_template("show")
+    end
+  end
+
+
+  describe "GET root_url" do
+    it "returns chats#index" do
+      expect(get: root_url).to route_to("chats#index")
     end
   end
 
