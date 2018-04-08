121e1597d00c9ec8f6aa537d777fccd19d5c9107
ramazan yaraliev <yaru@bk.ru>
2018-04-08 21:35:24 +0300

    chats_page_spec: The visitor should see a chats list

--- spec/features/chats_pages_spec.rb
+++ spec/features/chats_pages_spec.rb
@@ -13,4 +13,13 @@ RSpec.feature "Visiting the chats page", type: :feature do
     visit "chats/#{chat.id}"
     expect(page).to have_text(message.message)
   end
+
+  scenario "The visitor should see a chats list" do
+    chat = Chat.create(
+        chat_id: "123456",
+        first_name: "Pavel"
+    )
+    visit "chats/#{chat.id}"
+    expect(page).to have_text(chat.first_name)
+  end
 end
