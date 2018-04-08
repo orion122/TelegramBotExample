bc6fa7ce699e9dfa7854d8f4157669c45d68be13
ramazan yaraliev <yaru@bk.ru>
2018-04-08 21:32:57 +0300

    chats_page_spec: The visitor should see a saved message of current chat

---
+++ spec/features/chats_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Visiting the chats page", type: :feature do
+  scenario "The visitor should see a saved message of current chat" do
+    chat = Chat.create(
+        chat_id: "123456",
+        first_name: "Pavel"
+    )
+    message = chat.messages.create(
+        incoming: false,
+        message: "some message"
+    )
+    visit "chats/#{chat.id}"
+    expect(page).to have_text(message.message)
+  end
+end
