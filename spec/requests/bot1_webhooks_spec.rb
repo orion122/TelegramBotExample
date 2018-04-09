9fd94efc4d9cb7a6ae5ae838cdd4909c3f42fc56
ramazan <yaru@bk.ru>
2018-04-09 16:21:35 +0300

    add test: incoming telegram message matches with broadcast message

--- spec/requests/bot1_webhooks_spec.rb
+++ spec/requests/bot1_webhooks_spec.rb
@@ -1,9 +1,10 @@
 require 'telegram/bot/updates_controller/rspec_helpers'
+require 'rails_helper'
+require "action_cable/testing/rspec"
 
 RSpec.describe Bot1WebhooksController, :robo1bot do
   include_context 'telegram/bot/integration'
   include_context 'telegram/bot/updates_controller'
-
 
   describe '#start' do
     subject { -> { dispatch_command :start } }
@@ -11,12 +12,14 @@ RSpec.describe Bot1WebhooksController, :robo1bot do
   end
 
 
-=begin
   describe '#message' do
     subject { -> { dispatch_message text } }
     let(:text) { 'some plain text' }
-    it { should respond_with_message "You wrote: #{text}" }
+    it "matches with broadcast message" do
+      expect {
+        ActionCable.server.broadcast('telegram', message: text, chat_id: from['id'])
+      }.to have_broadcasted_to('telegram').from_channel(ChatChannel).with(message: text, chat_id: nil)
+    end
   end
-=end
 
 end
