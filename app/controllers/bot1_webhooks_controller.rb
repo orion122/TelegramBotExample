830e72d4d5c06558280d96d05d31e1755bc40a35
ramazan <yaru@bk.ru>
2018-04-10 11:10:29 +0300

    checking module

--- app/controllers/bot1_webhooks_controller.rb
+++ app/controllers/bot1_webhooks_controller.rb
@@ -4,17 +4,5 @@ class Bot1WebhooksController < Telegram::Bot::UpdatesController
 
   @@bot = 'robo1bot'
 
-  def start(data = nil, *)
-    unless Chat.exists?(chat_id: from['id'])
-      Chat.create(chat_id: from['id'], first_name: from['first_name'])
-    end
-
-    respond_with :message, text: "Hello, #{from['first_name']}"
-  end
-
-
-  def message(message)
-    chat = Chat.where(chat_id: message['from']['id']).first
-    chat.messages.create(message: message['text'], bot: @@bot)
-  end
+  include Bots
 end
--- app/controllers/bots.rb
+++ app/controllers/bots.rb
@@ -1,26 +1,15 @@
 module Bots
-  def start(data = nil, *)
+  def start(*)
     unless Chat.exists?(chat_id: from['id'])
-      chat = Chat.new(
-          chat_id: from['id'],
-          first_name: from['first_name'])
-      chat.save
-
-      ActionCable.server.broadcast 'telegram', message: "New user: #{from['first_name']}"
+      Chat.create(chat_id: from['id'], first_name: from['first_name'])
     end
 
     respond_with :message, text: "Hello, #{from['first_name']}"
   end
 
 
-
   def message(message)
-    ActionCable.server.broadcast 'telegram',
-                                 message: "#{message['from']['first_name']}: #{message['text']}",
-                                 chat_id: message['from']['id']
-
     chat = Chat.where(chat_id: message['from']['id']).first
-    message = chat.messages.create(message: message['text'], bot: @bot)
-    message.save
+    chat.messages.create(message: message['text'], bot: class_variable_get(bot))
   end
 end
