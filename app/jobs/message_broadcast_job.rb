80a5e8be9cb367132902baf9d5224551b271534a
ramazan <yaru@bk.ru>
2018-04-10 10:24:44 +0300

    fix first_name: if incoming then 'me'

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,8 +2,10 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
+    first_name = message.incoming ? 'me' : message.chat.first_name
+
     ActionCable.server.broadcast 'telegram',
-                                 message: "#{message.chat.first_name}: #{message.message}",
+                                 message: "#{first_name}: #{message.message}",
                                  chat_id: message.chat.chat_id
   end
 end
