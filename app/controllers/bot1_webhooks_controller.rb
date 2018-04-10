36796096c20c763c30bb432e7906f2156943beb8
ramazan <yaru@bk.ru>
2018-04-10 10:36:22 +0300

    fix class name

--- app/controllers/bot1_webhooks_controller.rb
+++ app/controllers/bot1_webhooks_controller.rb
@@ -1,4 +1,4 @@
-class Bot2WebhooksController < Telegram::Bot::UpdatesController
+class Bot1WebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   context_to_action!
 
