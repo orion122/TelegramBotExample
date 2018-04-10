c7c6aacec4c266edcc396f41796d4bab3cf0dff8
root <yaru@bk.ru>
2018-04-10 11:53:50 +0300

    override initialize and include module

--- app/controllers/bot1_webhooks_controller.rb
+++ app/controllers/bot1_webhooks_controller.rb
@@ -2,19 +2,10 @@ class Bot1WebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   context_to_action!
 
-  @@bot = 'robo1bot'
-
-  def start(data = nil, *)
-    unless Chat.exists?(chat_id: from['id'])
-      Chat.create(chat_id: from['id'], first_name: from['first_name'])
-    end
-
-    respond_with :message, text: "Hello, #{from['first_name']}"
+  def initialize(*)
+    super
+    @bot = 'robo1bot'
   end
 
-
-  def message(message)
-    chat = Chat.where(chat_id: message['from']['id']).first
-    chat.messages.create(message: message['text'], bot: @@bot)
-  end
+  include Bot
 end
--- app/controllers/bot2_webhooks_controller.rb
+++ app/controllers/bot2_webhooks_controller.rb
@@ -2,19 +2,10 @@ class Bot2WebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   context_to_action!
 
-  @@bot = 'robo6bot'
-
-  def start(data = nil, *)
-    unless Chat.exists?(chat_id: from['id'])
-      Chat.create(chat_id: from['id'], first_name: from['first_name'])
-    end
-
-    respond_with :message, text: "Hello, #{from['first_name']}"
+  def initialize(*)
+    super
+    @bot = 'robo6bot'
   end
 
-
-  def message(message)
-    chat = Chat.where(chat_id: message['from']['id']).first
-    chat.messages.create(message: message['text'], bot: @@bot)
-  end
+  include Bot
 end
