0ffb2208de52295369609561a97f2f9757ee2956
ramazan <yaru@bk.ru>
2018-04-10 10:35:20 +0300

    remove trash

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -8,7 +8,6 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def reply(data)
-    my_name = data['my_name']
     msg     = data['message']
     chat_id = data['chat_id']
 
@@ -16,14 +15,7 @@ class ChatChannel < ApplicationCable::Channel
 
     Telegram.bots[last_bot.to_sym].send_message(chat_id: data['chat_id'], text: msg)
 
-=begin
-    ActionCable.server.broadcast 'telegram',
-                                 message: "#{my_name}: #{msg}",
-                                 chat_id: chat_id
-=end
-
     chat = Chat.where(chat_id: chat_id).first
-    message = chat.messages.create(incoming: false, message: msg)
-    message.save
+    chat.messages.create(incoming: false, message: msg)
   end
 end
--- app/controllers/bot2_webhooks_controller.rb
+++ app/controllers/bot2_webhooks_controller.rb
@@ -6,30 +6,15 @@ class Bot2WebhooksController < Telegram::Bot::UpdatesController
 
   def start(data = nil, *)
     unless Chat.exists?(chat_id: from['id'])
-      chat = Chat.new(
-          chat_id: from['id'],
-          first_name: from['first_name'])
-      chat.save
-
-      # ActionCable.server.broadcast 'telegram',
-      #                              message: "New user",
-      #                              chat_id: from['id']
+      Chat.create(chat_id: from['id'], first_name: from['first_name'])
     end
 
     respond_with :message, text: "Hello, #{from['first_name']}"
   end
 
 
-
   def message(message)
-=begin
-    ActionCable.server.broadcast 'telegram',
-                                 message: "#{message['from']['first_name']}: #{message['text']}",
-                                 chat_id: message['from']['id']
-=end
-
     chat = Chat.where(chat_id: message['from']['id']).first
-    message = chat.messages.create(message: message['text'], bot: @@bot)
-    message.save
+    chat.messages.create(message: message['text'], bot: @@bot)
   end
 end
